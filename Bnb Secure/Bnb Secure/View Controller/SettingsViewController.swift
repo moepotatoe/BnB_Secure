3de358baffb095b80621d3f596538cb7da0f690e
Maia Nguyen <maialyn@csu.fullerton.edu>
2022-02-11 22:28:01 -0800

    Updated Settings UI

--- Bnb Secure/Bnb Secure/View Controller/SettingsViewController.swift
+++ Bnb Secure/Bnb Secure/View Controller/SettingsViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var licenseTextField: UITextField!
+    @IBOutlet weak var updateChangesButton: UIButton!
+    @IBOutlet weak var logOutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func homeButtonTapped(_ sender: UIButton) {
+        self.showRefresh()
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (t) in
+            self.stopRefresh()
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
